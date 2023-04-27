013c87c9c1ebdebd5f58f7585cf2c25bc2ee3d20
CreepyPvP <interruptedThread@gmail.com>
2023-04-27 20:34:25 +0200

    Bugfix

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ cp ./config/tmux.conf ~/.tmux.conf
 # alacritty setup
 mkdir ~/.config/alacritty
 cp -R ./config/alacritty/ ~/.config/
-git clone https://github.com/eendroroy/alacritty-theme.git ~/.alacritty-colorscheme
+git clone https://github.com/eendroroy/alacritty-theme.git ~/.config/alacritty/themes
 
 # git setup
 cp ./config/gitconfig ~/.gitconfig
