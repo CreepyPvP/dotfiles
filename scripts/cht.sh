9457812320296f8617e86afe9e188c4f0a38d30a
Luis Dietl <interruptedThread@gmail.com>
2023-05-29 01:46:54 +0200

    The commit you deserve

---
+++ scripts/cht.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/.tmux-cht-languages; then
+    query=`echo $query | tr ' ' '+'`
+    curl -s cht.sh/$selected/$query | less -r
+else
+    curl -s cht.sh/$selected~$query | less -r
+fi
