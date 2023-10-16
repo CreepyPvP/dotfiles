e229f05ca047fa5690983bd5336ccb56a64991ec
Luis Dietl <interruptedThread@gmail.com>
2023-10-16 17:53:22 +0200

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm neovim tmux alacritty git docker rustup \
-	firefox neofetch ripgrep nodejs cmake npm fzf picom docker-compose polybar \
-    rofi
+sudo pacman -S --noconfirm neovim tmux alacritty git rustup \
+	firefox neofetch ripgrep cmake fzf rofi
 
 # font 
 sudo pacman -S --noconfirm extra/ttf-cascadia-code-nerd
@@ -18,7 +17,7 @@ cp ./config/zshrc ~/.zshrc
 
 
 # neovim setup
-git clone https://github.com/CreepyPvP/VimSetup ~/.config/nvim
+git clone https://github.com/CreepyPvP/nvim-win-config ~/.config/nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
@@ -28,23 +27,13 @@ cp ./config/tmux.conf ~/.tmux.conf
 # alacritty setup
 mkdir ~/.config/alacritty
 cp -R ./config/alacritty/ ~/.config/
-git clone https://github.com/eendroroy/alacritty-theme.git ~/.config/alacritty/themes
 
 # git setup
 cp ./config/gitconfig ~/.gitconfig
 
 # i3 setup
 cp ./config/i3 ~/.config/ -r
-cp ./config/rofi ~/.config/rofi -r
 cp ./config/picom ~/.config/picom -r
-cp ./config/polybar ~/.config/polybar -r
 
 # scripts
 sudo install ./scripts/sessionizer.sh /usr/bin
-sudo install ./scripts/cht.sh /usr/bin
-# todo: move tmux commands + tmux languages
-
-# starship setup
-# rustup default stable
-# cp ./config/starship.toml ~/.config/starship.toml
-# echo "eval \"$(starship init bash)\"" >> ~/.bashrc
